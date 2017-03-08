8331fffdd3a495454e5b2efd55cb7c6f25deccef
Carl Luo <cluo@jobs2careers.com>
2017-03-08 04:03:14 -0600

    comments out the codes

--- src/main/java/com/sillycat/analyzerjava/ExecutorApp.java
+++ src/main/java/com/sillycat/analyzerjava/ExecutorApp.java
@@ -1,12 +1,16 @@
-//package com.sillycat.analyzerjava;
-//
-//import com.sillycat.analyzerjava.perf.DFAAnalyzerBenchmark;
-//import com.sillycat.analyzerjava.perf.SeparateRegexAnalyzerBenchmark;
-//import com.sillycat.analyzerjava.perf.SingleRegexAnalyzerBenchmark;
-//
-//public class ExecutorApp {
-//
-//	public static void main(String[] args) {
+package com.sillycat.analyzerjava;
+
+import java.io.IOException;
+
+import org.apache.solr.client.solrj.SolrServerException;
+
+import com.sillycat.analyzerjava.perf.DFAAnalyzerBenchmark;
+import com.sillycat.analyzerjava.perf.SeparateRegexAnalyzerBenchmark;
+import com.sillycat.analyzerjava.perf.SingleRegexAnalyzerBenchmark;
+
+public class ExecutorApp {
+
+	public static void main(String[] args) throws SolrServerException, IOException {
 //		/*
 //		 * Regex may not match all the requirement in the future
 //		 */
@@ -17,6 +21,7 @@
 //		 * DFA, put all the keyword in the dictionary
 //		 */
 //		DFAAnalyzerBenchmark.main(args);
-//	}
-//
-//}
+		SolrMainApp.main(args);
+	}
+
+}
