c56520a5cfcce8965b5adb1fe490f85ec77bdf71
Carl Luo <cluo@jobs2careers.com>
2017-03-08 04:00:37 -0600

    comments out the codes

--- src/main/java/com/sillycat/analyzerjava/ExecutorApp.java
+++ src/main/java/com/sillycat/analyzerjava/ExecutorApp.java
@@ -1,22 +1,22 @@
-package com.sillycat.analyzerjava;
-
-import com.sillycat.analyzerjava.perf.DFAAnalyzerBenchmark;
-import com.sillycat.analyzerjava.perf.SeparateRegexAnalyzerBenchmark;
-import com.sillycat.analyzerjava.perf.SingleRegexAnalyzerBenchmark;
-
-public class ExecutorApp {
-
-	public static void main(String[] args) {
-		/*
-		 * Regex may not match all the requirement in the future
-		 */
-		SingleRegexAnalyzerBenchmark.main(args);
-		SeparateRegexAnalyzerBenchmark.main(args);
-		
-		/**
-		 * DFA, put all the keyword in the dictionary
-		 */
-		DFAAnalyzerBenchmark.main(args);
-	}
-
-}
+//package com.sillycat.analyzerjava;
+//
+//import com.sillycat.analyzerjava.perf.DFAAnalyzerBenchmark;
+//import com.sillycat.analyzerjava.perf.SeparateRegexAnalyzerBenchmark;
+//import com.sillycat.analyzerjava.perf.SingleRegexAnalyzerBenchmark;
+//
+//public class ExecutorApp {
+//
+//	public static void main(String[] args) {
+//		/*
+//		 * Regex may not match all the requirement in the future
+//		 */
+//		SingleRegexAnalyzerBenchmark.main(args);
+//		SeparateRegexAnalyzerBenchmark.main(args);
+//		
+//		/**
+//		 * DFA, put all the keyword in the dictionary
+//		 */
+//		DFAAnalyzerBenchmark.main(args);
+//	}
+//
+//}
